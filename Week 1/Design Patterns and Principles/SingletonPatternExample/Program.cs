dfaf827537edec452873ff0b6d6a512b5982df47
Suraj K P <surajkp3685@gmail.com>
2025-06-18 22:39:28 +0530

    Singleton Pattern Mandatory HandsOn was Submitted

--- Week 1/Design Patterns and Principles/SingletonPatternExample/Program.cs
+++ Week 1/Design Patterns and Principles/SingletonPatternExample/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace SingletonPatternExample
 {
-    internal class Program
+    sealed class Logger // Implemeted sealed class which prevents from being inherited to other classes in the application
+    {
+        private static Logger _instance = new Logger();
+        private Logger()
+        {
+            Console.WriteLine("Private Static Logger Instance and Private Constructor was created.");
+        }
+        public static Logger Instance 
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+    class Program // Main class to test the Singleton Pattern Design
     {
         static void Main(string[] args)
         {
+            Logger logger1 =Logger.Instance;
+            Logger logger2 =Logger.Instance;
+            if (logger1 == logger2)
+            {
+                Console.WriteLine("The Singleton Pattern Design was Succesful: Only one Instance created Throughout the Process. ");
+            }
+            else
+            {
+                Console.WriteLine(" Pattern Design Un-successful: More than one Instance is created");
+            }
+            Console.ReadLine();// Terminal gets closed when pressed enter, to prevent instant closing.
         }
     }
+
 }
